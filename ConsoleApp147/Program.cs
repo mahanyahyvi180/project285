45b4d498f343ecd6ee8e4750f1e63bd771cde309
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-12-23 21:56:48 +0330

    Add files via upload

---
+++ ConsoleApp147/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp147
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var string1 = "hello there";
+            var characterArray = new char[5];
+
+            Console.WriteLine($"string1:\"{string1}\"");
+
+            Console.WriteLine($"Length of string1:{string1.Length}");
+
+            Console.Write("The string reserved is:");
+
+            for (int i = string1.Length - 1; i >= 0; --i)
+            {
+                Console.Write(string1[i]);
+            }
+            string1.CopyTo(0, characterArray, 0, characterArray.Length);
+            Console.Write("\n the character array is:");
+
+            foreach (var element in characterArray)
+            {
+                Console.Write(element);
+            }
+            Console.WriteLine("\n");
+            Console.ReadLine();
+
+        }
+    }
+}
